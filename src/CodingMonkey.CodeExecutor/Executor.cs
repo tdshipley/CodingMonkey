2121f10d27f6bae653373ff66752d54d4741553e
Thomas Shipley <tshipley@neworbit.co.uk>
2016-03-06 11:05:33 +0000

    inital commit

---
+++ src/CodingMonkey.CodeExecutor/Executor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingMonkey.CodeExecutor
+{
+    public class Executor
+    {
+        public Executor()
+        {
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public AppDomain GetAppDomain()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
