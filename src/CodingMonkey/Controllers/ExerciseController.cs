3518c4e03b2e44533c1d453656b12ae6b355d7d7
Thomas Shipley <thomas.shipley@googlemail.com>
2016-03-27 14:40:40 +0100

    Updated global json to target correct framework so intellisense in vs code works

--- src/CodingMonkey/Controllers/ExerciseController.cs
+++ src/CodingMonkey/Controllers/ExerciseController.cs
@@ -6,9 +6,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.Data.Entity;
-    using Microsoft.Data.Entity.Storage;
 
     public class ExerciseController : ApiController
     {
