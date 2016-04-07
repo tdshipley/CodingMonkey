f9a74a3b03dbe09aba6f1f651731573c96dba4d5
Thomas Shipley <tshipley@neworbit.co.uk>
2016-04-07 17:37:55 +0100

    Part way setting up navigation

---
+++ src/CodingMonkey/src/admin/exercise/create.js
@@ -0,0 +1,5 @@
+﻿export class create {
+    constructor() {
+        this.heading = "Create Exercise";   
+    }
+}
--- src/CodingMonkey/src/app.js
+++ src/CodingMonkey/src/app.js
@@ -1,9 +1,39 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'code/editor',   name: 'CodeEditor',   moduleId: 'code/editor',  nav: true, title: 'Code Editor'}
+    config.title = 'Coding Monkey';
+    config.map(
+        [
+            {
+                settings: {
+                    group: 'root'
+                },
+                route: ['', 'welcome'],
+                name: 'welcome',
+                moduleId: 'welcome',
+                nav: true,
+                title: 'Welcome'
+            },
+            {
+                settings: {
+                    group: 'root'
+                },
+                route: 'code/editor',  
+                name: 'CodeEditor',
+                moduleId: './code/editor',
+                nav: true,
+                title: 'Code Editor'
+            },
+            // Admin
+            {
+                settings: {
+                    group: 'admin'
+                },
+                route: 'admin/exercise/create',
+                name: 'ExerciseCreate', 
+                moduleId: './admin/exercise/create',
+                nav: true,
+                title: "Exercise Create"
+            }
     ]);
 
     this.router = router;
