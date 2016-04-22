813e648eeff33b0a01f45a27bcfd91e7d02b10a7
Thomas Shipley <thomas.shipley@googlemail.com>
2016-04-22 17:28:25 +0100

    Tweaked the test details page and fixed a couple of minor bugs

--- src/CodingMonkey/src/admin/test/details.js
+++ src/CodingMonkey/src/admin/test/details.js
@@ -1,15 +1,17 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient, json} from 'aurelia-fetch-client';
+import {Router} from 'aurelia-router';
 import 'fetch';
 import toastr from 'toastr';
 
-@inject(HttpClient)
+@inject(HttpClient, Router)
 export class details {
-        constructor(http) {
+        constructor(http, router) {
         var loc = window.location;
         
         this.heading = "Test Details";
         this.baseUrl = loc.protocol + "//" + loc.host;
+        this.appRouter = router;
         
         this.notify = toastr;
         this.notify.options.progressBar = true;
@@ -28,9 +30,15 @@ export class details {
                 testInputs: []
             }
         }
+        
+        this.exerciseId = 0;
+        this.exerciseTemplateId = 0;
     }
     
     activate(params) {
+        this.exerciseId = params.exerciseId;
+        this.exerciseTemplateId = params.exerciseTemplateId;
+        
         this.http.baseUrl = this.baseUrl + '/api/Exercise/' + params.exerciseId + '/Test/';
         
         this.http.fetch('details/' + params.id)
@@ -56,8 +64,11 @@ export class details {
               }
           })
           .catch(err => {
-              console.log(err);
               this.notify.error("Failed to get test.")
           });
+    }
+    
+    goToTestList() {
+        this.appRouter.navigate("admin/exercise/" + this.exerciseId + "/" + this.exerciseTemplateId + "/tests");
     }
 }
