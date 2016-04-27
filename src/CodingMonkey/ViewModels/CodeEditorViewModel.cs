787ae99d1f994d95a9daca59a3265b9495cb99c3
Thomas Shipley <tshipley@neworbit.co.uk>
2016-04-27 16:46:47 +0100

    Updated the view model to have test results on code editor

--- src/CodingMonkey/ViewModels/CodeEditorViewModel.cs
+++ src/CodingMonkey/ViewModels/CodeEditorViewModel.cs
@@ -15,6 +15,22 @@ namespace CodingMonkey.ViewModels
 
         public bool HasErrors { get; set; }
         public IList<CompilerErrorViewModel> Errors { get; set; }
+        public IList<TestResultViewModel> TestResults { get; set; }
         public object Output { get; set; }
+    }
+
+    public class TestResultViewModel
+    {
+
+        public string Description { get; set; }
+        public IList<TestResultInputViewModel> Inputs { get; set; } 
+        public object ExpectedOutput { get; set; }
+        public object ActualOutput { get; set; }
+    }
+
+    public struct TestResultInputViewModel
+    {
+        public string Argumentname { get; set; }
+        public string Value { get; set; }
     }
 }
