9dbbac07eb53772d83fba0a1aa15c8db68a25448
Thomas Shipley <tshipley@neworbit.co.uk>
2016-04-28 17:29:15 +0100

    Added a skeleton for a Authentication controller

---
+++ src/CodingMonkey/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿namespace CodingMonkey.Controllers
+{
+    using System;
+
+    using CodingMonkey.ViewModels;
+    using CodingMonkey.Models;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Mvc;
+
+    [Route("api/[controller]/[action]")]
+    public class AuthenticationController
+    {
+        [FromServices]
+        private SignInManager<ApplicationUser> _signInManager { get; set; }
+
+        [HttpPost]
+        public JsonResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public JsonResult Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
