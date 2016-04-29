d21c43539ff240ab97a44951ba7520619e4528ea
Thomas Shipley <tshipley@neworbit.co.uk>
2016-04-29 10:59:35 +0100

    Setup login and logout methods on the authentication controller

--- src/CodingMonkey/Controllers/AuthenticationController.cs
+++ src/CodingMonkey/Controllers/AuthenticationController.cs
@@ -1,6 +1,7 @@
 ﻿namespace CodingMonkey.Controllers
 {
     using System;
+    using System.Threading.Tasks;
 
     using CodingMonkey.ViewModels;
     using CodingMonkey.Models;
@@ -9,21 +10,44 @@
     using Microsoft.AspNet.Mvc;
 
     [Route("api/[controller]/[action]")]
-    public class AuthenticationController
+    public class AuthenticationController : Controller
     {
         [FromServices]
-        private SignInManager<ApplicationUser> _signInManager { get; set; }
+        public SignInManager<ApplicationUser> _signInManager { get; set; }
 
         [HttpPost]
-        public JsonResult Login()
+        public async Task<JsonResult> Login([FromBody]LoginViewModel vm)
         {
-            throw new NotImplementedException();
+            var response = new LoginResultViewModel();
+
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, lockoutOnFailure: false);
+                response.LoginSucceeded = result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                response.LoginSucceeded = false;
+            }
+
+            return Json(response);
         }
 
         [HttpPost]
-        public JsonResult Logout()
+        public async Task<JsonResult> Logout()
         {
-            throw new NotImplementedException();
+            var response = new LoginResultViewModel();
+            try
+            {
+                await _signInManager.SignOutAsync();
+                response.LogoutSucceeded = true;
+            }
+            catch (Exception)
+            {
+                response.LogoutSucceeded = true;
+            }
+
+            return Json(response);
         }
 
     }
---
+++ src/CodingMonkey/ViewModels/LoginResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingMonkey.ViewModels
+{
+    public class LoginResultViewModel
+    {
+        public bool? LoginSucceeded { get; set; }
+
+        public bool? LogoutSucceeded { get; set; }
+    }
+}
