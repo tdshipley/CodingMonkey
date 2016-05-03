8a2acb60457d024b8371eadf3d0b74bb463589e6
Thomas Shipley <tshipley@neworbit.co.uk>
2016-05-03 17:31:26 +0100

    Adding an inline view to logout controller

--- src/CodingMonkey/src/authentication/logout.js
+++ src/CodingMonkey/src/authentication/logout.js
@@ -4,6 +4,7 @@ import {Router} from 'aurelia-router';
 import 'fetch';
 import toastr from 'toastr';
 
+@inlineView("<template></template>")
 @inject(HttpClient, Router)
 export class logout {
     constructor(http, router) {
