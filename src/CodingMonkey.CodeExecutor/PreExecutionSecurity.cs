9b9bcca724646b63dc07c796129a4ace80cefd49
Thomas Shipley <tshipley@neworbit.co.uk>
2016-05-04 13:57:45 +0100

    Added a pre execution security policy. For now this lives in code - although it could and perhaps should be transalted to the DB at a later date

---
+++ src/CodingMonkey.CodeExecutor/PreExecutionSecurity.cs
@@ -0,0 +1,52 @@
+﻿namespace CodingMonkey.CodeExecutor
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class PreExecutionSecurity
+    {
+        /// <summary>
+        /// A list of namespaces which are allowed to be included in using statements by
+        /// user submitted code.
+        /// </summary>
+        private static readonly IList<string> AllowedNamespaces = new ReadOnlyCollection<string>
+            (new List<string>()
+                 {
+                     "System",
+                     "System.Collections.Generic"
+                 });
+
+        public string SanitiseCode(string codeToSanitise)
+        {
+            string sanitisedCode;
+
+            sanitisedCode = SanitiseUsings(codeToSanitise);
+
+            return sanitisedCode;
+        }
+
+        private string SanitiseUsings(string codeToSanitise)
+        {
+            string sanitisedCode = codeToSanitise;
+
+            const string UsingPattern = "using.+;";
+            sanitisedCode = Regex.Replace(sanitisedCode, UsingPattern, "");
+
+            IList<string> usingStatements = this.GetAllowedUsingStatements();
+
+            string usingStatementsFlaterned = string.Join(Environment.NewLine, usingStatements);
+
+            sanitisedCode = usingStatementsFlaterned + Environment.NewLine + sanitisedCode;
+
+            return sanitisedCode;
+        }
+
+        private IList<string> GetAllowedUsingStatements()
+        {
+            return AllowedNamespaces.Select(x => x = "using " + x + ";").ToList();
+        }
+    }
+}
