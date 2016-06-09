195632c916428ab8253f44c90a2ff517e5767457
Thomas Shipley <tshipley@neworbit.co.uk>
2016-06-09 15:21:20 +0100

    Move repo to the correct folder

--- src/CodingMonkey/Models/Repositories/ExerciseRepository.cs
+++ src/CodingMonkey/Models/Repositories/ExerciseRepository.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CodingMonkey.Models
+namespace CodingMonkey.Models.Repositories
 {
-    using CodingMonkey.Models.Repositories;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Caching.Memory;
 
