e5f2823d310a580bd662a7a15f016d39d831ba18
Thomas Shipley <thomas.shipley@just-eat.com>
2017-03-08 15:29:18 +0000

    Switch on development logging to figure out what is broken

--- src/CodingMonkey/src/main.js
+++ src/CodingMonkey/src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
+    .developmentLogging()
     .plugin('aurelia-dialog');
 
   //Uncomment the line below to enable animation.
