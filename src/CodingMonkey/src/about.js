e9f78bb25b23e9ff2db1aec3748f564c112a1803
thomasshipley-je <thomas.shipley@just-eat.com>
2018-01-17 18:32:34 +0000

    Remove contributors...

--- src/CodingMonkey/src/about.js
+++ src/CodingMonkey/src/about.js
@@ -9,8 +9,6 @@ export class About {
     constructor(http, router) {
         this.appRouter = router;
         this.heading = 'About Coding Monkey';
-        this.contributorUserProfiles = [];
-        this.contributorUserNames = ["captainkirk854", "bretcolloff", "chreden", "jonparish", "no1melman"];
 
         http.configure(config => {
             config
@@ -21,23 +19,5 @@ export class About {
     }
 
     activate() {
-        this.getContributorProfiles();
-    }
-
-    getContributorProfiles() {
-        this.http.fetch("../../json/contributors.json")
-            .then(response => response.json())
-            .then(contributors => {
-                for (let contributor of contributors) {
-                    contributor.displayName = "";
-                    if (contributor.name === undefined) {
-                        contributor.displayName = contributor.login;
-                    } else {
-                        contributor.displayName = contributor.name + " (" + contributor.login + ")";
-                    }
-
-                    this.contributorUserProfiles.push(contributor);
-                }
-            });
     }
 }
